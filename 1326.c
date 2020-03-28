7fd4018e64339df5e5ca87e7afab7fb2ff72b6b6
zjykzk <zjykzk@gmail.com>
2020-03-28 14:55:17 +0800

    1326

---
+++ 1326.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+int nt[10001][100];
+
+int minTaps(int n, int* ranges, int rangesSize){
+  memset(nt, 0, sizeof(nt));
+
+  for (int j = 0; j < rangesSize; j++) {
+    for (int i = 0, r = ranges[j]; i <= r; i++) {
+
+    }
+  }
+}
+
+int main() {
+
+}
